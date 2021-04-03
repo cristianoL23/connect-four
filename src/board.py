8c79e2a3ad9fa5726b459b2372e354cbb5b70895
Lucy Christian <lchri@MacBook-Pro.phub.net.cable.rogers.com>
2021-04-03 16:50:00 -0400

    Formatting and adding check winning move for vertical and horizontal

--- src/board.py
+++ src/board.py
@@ -3,32 +3,34 @@ class Board:
         self.rows = 6
         self.cols = 7
         self.board = [[0 for i in range(self.cols)] for i in range(self.rows)]
-        
+
     def getRows(self):
         return self.rows
-    
+
     def getCols(self):
         return self.cols
-    
+
     def getBoard(self):
         return self.board
-    
+
     def setBoard(self, board):
         self.board = board
-    
+
     def printBoard(self):
         board = self.getBoard()
-        print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-        for row in board]))
-    
+        print('\n'.join([''.join(['{:4}'.format(colItem) for colItem in row])
+                         for row in board]))
+
     def placeMove(self, move, turn):
         board = self.getBoard()
-        rowVal = self.getRows() -1
-        
-        for i in range(rowVal, 0, -1):
-            if board[i][move] == 0:
-                board[i][move] = turn
+        rowVal = self.getRows() - 1
+
+        for row in range(rowVal, -1, -1):
+            if board[row][move] == 0:
+                board[row][move] = turn
                 self.setBoard(board)
+                currentRow = row
                 break
             else:
                 continue
+        return currentRow
--- src/game.py
+++ src/game.py
@@ -1,60 +1,101 @@
 from board import Board
 
+
 class Game:
 
     def __init__(self):
         self.turn = 1
         self.board_class = Board()
-    
+
     def runGame(self):
         self.printTurn()
         self.board_class.printBoard()
-        
+
         win = self.handleMove()
         while(win != 4):
             self.alternateTurn()
             self.printTurn()
             self.board_class.printBoard()
             win = self.handleMove()
-        print("Player" + str(self.turn) + "had won the game")
-    
+        print("Player " + str(self.turn) + " has won the game")
+
     def handleMove(self):
-        move = input("Enter your move based on columns: ")
-        self.board_class.placeMove(int(move), self.turn)
-        win = self.checkForWin(int(move))
+        move = input("Enter your move based on columns (0-6): ")
+        currentRow = self.board_class.placeMove(int(move), self.turn)
+        win = self.checkForWin(int(move), currentRow)
         return win
-        
+
     def printTurn(self):
-        print("Player" + str(self.turn) + "\'s turn")
-        
+        print("Player " + str(self.turn) + "\'s turn")
+
     def alternateTurn(self):
         if(self.turn == 1):
             self.turn = 2
         else:
             self.turn = 1
 
-    #TODO: finsih win checking algorithm
-    def checkForWin(self, col):
-    #     #4 of the same number in a line vertical, horizontal, or 
-    #     #to check above board[col][i + 1]
-    #     #to check below board[col][i - 1]
-    #     #to check left board[col -1][i]
-    #     #to check right board[col + 1][i]
-    #     #Diagonals
-    #     #to check up-left board[col -1][i + 1] 
-    #     #to check up-right board[col +1][i+ ]
-    #     #to check down-left board[col -1][i- 1]
-    #     #o check down-right board[col +1][i- 1]
-
-    #     #checking for win along a row
+    # TODO: finsih win checking algorithm
+    def checkForWin(self, moveCol, currentRow):
+        #     #4 of the same number in a line vertical, horizontal, or
+        #     #to check above board[col][i + 1]
+        #     #to check below board[col][i - 1]
+        #     #to check left board[col -1][i]
+        #     #to check right board[col + 1][i]
+        #     #Diagonals
+        #     #to check up-left board[col -1][i + 1]
+        #     #to check up-right board[col +1][i+ ]
+        #     #to check down-left board[col -1][i- 1]
+        #     #o check down-right board[col +1][i- 1]
+        board = self.board_class.getBoard()
         win = 0
-        # row = board[col].index(turn)
-    
-        # for col in range(7):
-        #     if board[col][row] == turn:
-        #         win += 1
-        #     else:
-        #         win = 0
-        
-        #for row in range(6):
+
+        # check win vertically
+        for row in range(self.board_class.getRows()):
+            if board[row][moveCol] == self.turn:
+                win += 1
+                if win == 4:
+                    return win
+            else:
+                win = 0
+
+        # check win horizontally
+        for col in range(self.board_class.getCols()):
+            if board[currentRow][col] == self.turn:
+                win += 1
+                if win == 4:
+                    return win
+            else:
+                win = 0
+
+        # for c in range(COLUMN_COUNT-3):
+		# for r in range(ROW_COUNT-3):
+		# 	if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+		# 		return True
+
+        # check win positive diagonal
+        for c in range(moveCol, self.board_class.getCols(), 1):
+            for r in range(currentRow, -1, -1):
+                if r - 3 <= self.board_class.getRows() and c + 3 <= self.board_class.getCols():
+                    if board[r][c] == self.turn and board[r-1][c+1] == self.turn and board[r-2][c+2] == self.turn and board[r-3][c+3] == self.turn:
+                        print("there is a diagonal")
+                        win = 4
+                    else: 
+                        win = 0
+                # if board[r][c] == self.turn:
+                #     win += 1
+                #     print(win)
+                # else:
+                #     win = 0 
+                #     print(win)
+                
+        # if board[currentRow][moveCol] == self.turn and board[currentRow + 1][moveCol + 1] == self.turn and board[currentRow + 2][moveCol + 2] == self.turn and board[currentRow + 3][moveCol + 3] == self.turn:
+        #     win = 4
+
+        # for row in range(self.board_class.getRows()):
+        #     for cols in range(self.board_class.getCols):
+        #         if board[moveCol][row] == self.turn:
+        #             win += 1
+        #         else:
+        #             win = 0
+
         return win
